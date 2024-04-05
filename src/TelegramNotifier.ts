3123d9df2ab54d47c8dd6c654af57f97cd68b06c
Beezy <anonbeezy@proton.me>
2024-04-05 16:46:39 +0800

    feat: support markdown messages

--- src/TelegramNotifier.ts
+++ src/TelegramNotifier.ts
@@ -3,6 +3,8 @@ import type TelegramBot from 'node-telegram-bot-api'
 export class TelegramNotifier {
   constructor(private bot: TelegramBot, private chatId: TelegramBot.ChatId) {}
   notify(message: string) {
-    return this.bot.sendMessage(this.chatId, message)
+    return this.bot.sendMessage(this.chatId, `${message}`, {
+      parse_mode: 'MarkdownV2',
+    })
   }
 }
