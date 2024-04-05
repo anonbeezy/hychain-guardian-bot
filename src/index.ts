75514b9fab875e98d41453f489ff24722ed03b8c
Beezy <anonbeezy@proton.me>
2024-04-05 16:24:37 +0800

    feat: accept new command

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,22 @@ if (!guardianLogPath.startsWith('/')) {
   guardianLogPath = path.resolve(process.cwd(), process.env.GUARDIAN_LOG_PATH)
 }
 
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true })
+bot.onText(/\/debug_message/, (msg) => {
+  if (msg.is_topic_message) {
+    bot.sendMessage(msg.chat.id, JSON.stringify(msg, null, 2), {
+      message_thread_id: msg.message_thread_id,
+      reply_to_message_id: msg.message_id,
+    })
+  } else {
+    bot.sendMessage(msg.chat.id, JSON.stringify(msg, null, 2), {
+      reply_to_message_id: msg.message_id,
+    })
+  }
+})
+
 new GuardianSession(
   new Tail(process.env.GUARDIAN_LOG_PATH),
   new TelegramFormatter(),
-  new TelegramNotifier(
-    new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true }),
-    parseInt(process.env.CHAT_ID)
-  )
+  new TelegramNotifier(bot, process.env.GUARDIAN_CHAT_ID)
 ).start()
