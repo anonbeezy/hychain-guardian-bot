dcc460bd28f4be55caacd31c5e78f067f5e8e2e1
Beezy <anonbeezy@proton.me>
2024-04-05 16:23:52 +0800

    feat: accept number or string chat id

--- src/TelegramNotifier.ts
+++ src/TelegramNotifier.ts
@@ -1,7 +1,7 @@
 import type TelegramBot from 'node-telegram-bot-api'
 
 export class TelegramNotifier {
-  constructor(private bot: TelegramBot, private chatId: number) {}
+  constructor(private bot: TelegramBot, private chatId: TelegramBot.ChatId) {}
   notify(message: string) {
     return this.bot.sendMessage(this.chatId, message)
   }
