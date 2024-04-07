ce2d0349ed55384476a657b43ed3fca37601df4e
Beezy <anonbeezy@proton.me>
2024-04-07 18:05:02 +0800

    chore: remove console.log

--- src/TelegramFormatter.ts
+++ src/TelegramFormatter.ts
@@ -5,7 +5,6 @@ export class TelegramFormatter extends Formatter {
   format(executions: ExecutionWindow) {
     const execution = executions.last()
     const totalSumSubmittedAssertions = sumSubmittedAssertions(executions)
-    console.log(totalSumSubmittedAssertions)
 
     const statusEmoji = execution.submittedAssertions > 0 ? '🎉' : '💔'
     return `${statusEmoji} Submitted ${execution.submittedAssertions}/${
